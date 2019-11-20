53f640abf44cc322515b216ad4335edb040472b4
jeshocarmel <jeshocarmel@gmail.com>
2019-11-20 15:03:25 +0800

    solved question 10.9

--- ch10/sortedmatrixsearch.go
+++ ch10/sortedmatrixsearch.go
@@ -1,9 +1,12 @@
 package ch10
 
-import "fmt"
+import (
+	"math/rand"
+	"time"
+)
 
 //GenerateMatrix generates a matrix
-func GenerateMatrix(row int, col int) {
+func GenerateMatrix(row int, col int) [][]int {
 
 	mat := make([][]int, col)
 
@@ -11,6 +14,41 @@ func GenerateMatrix(row int, col int) {
 		mat[i] = make([]int, row)
 	}
 
-	fmt.Println(mat)
+	rand.Seed(time.Now().UnixNano())
+	random := rand.Intn(50)
 
+	for i := 0; i < len(mat); i++ {
+		for j := 0; j < len(mat[i]); j++ {
+
+			if i == 0 && j == 0 {
+				mat[i][j] = random
+			} else if j == 0 {
+				mat[i][j] = mat[i-1][j] + 5
+			} else if j > 0 {
+				mat[i][j] = mat[i][j-1] + 5
+			}
+
+		}
+	}
+
+	return mat
+}
+
+//SearchMatrix searches for an item in the sorted matrix
+func SearchMatrix(mat [][]int, x int) bool {
+
+	row := 0
+	col := len(mat[0]) - 1
+
+	for col >= 0 && row < len(mat) {
+		if mat[row][col] == x {
+			return true
+		} else if mat[row][col] > x {
+			col--
+		} else {
+			row++
+		}
+	}
+
+	return false
 }
--- ch10/sortedmatrixsearch_test.go
+++ ch10/sortedmatrixsearch_test.go
@@ -1,8 +1,48 @@
 package ch10
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestGenerateMatrix(t *testing.T) {
+	mat := GenerateMatrix(4, 4)
 
-	GenerateMatrix(3, 3)
+	for i := 0; i+1 < len(mat); i++ {
+		for j := 0; j+1 < len(mat[0]); j++ {
+			if mat[i+1][j] < mat[i][j] {
+				t.Error("condition fail in column asc sorting")
+				t.Fail()
+			}
+
+			if mat[i][j+1] < mat[i][j] {
+				t.Error("condition fail in row asc sorting")
+				t.Fail()
+			}
+		}
+	}
+}
+
+func TestSearchMatrix(t *testing.T) {
+
+	mat := GenerateMatrix(4, 4)
+	rand.Seed(time.Now().UnixNano())
+	sRow := rand.Intn(4)
+	sCol := rand.Intn(4)
+
+	isFound := SearchMatrix(mat, mat[sRow][sCol])
+
+	if !isFound {
+		t.Errorf("unable to find")
+		t.Fail()
+	}
+
+	isFound = SearchMatrix(mat, 100)
+
+	if isFound {
+		t.Errorf("invalid entry found")
+		t.Fail()
+	}
+
 }
