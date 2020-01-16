5731cd64cea0d0c45f30c7ea12cc349aff156f1c
jeshocarmel <jeshocarmel@gmail.com>
2020-01-16 14:05:13 +0800

    modified soln for 3.1

--- ch03/threeinone_test.go
+++ ch03/threeinone_test.go
@@ -10,24 +10,34 @@ func TestThreeInOne(t *testing.T) {
 
 	// creating three stacks numbered as 0,1,2 with a combined capacity of 12. each stack gets 4 as fixed allocation
 	mystack := newStack(3, 12)
+
+	//push into all the 3 stacks i.e. stack 0, stack and stack 2
 	mystack.push(0, 1)
 	mystack.push(1, 2)
 	mystack.push(2, 3)
 
-	assert.Equal(t, mystack.items, []int{1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0})
+	expected := []int{1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0}
+	assert.Equal(t, mystack.items, expected)
 
+	//push into stack 0 and 1
 	mystack.push(0, 4)
 	mystack.push(1, 5)
 
-	assert.Equal(t, mystack.items, []int{1, 4, 0, 0, 2, 5, 0, 0, 3, 0, 0, 0})
+	expected = []int{1, 4, 0, 0, 2, 5, 0, 0, 3, 0, 0, 0}
+	assert.Equal(t, mystack.items, expected)
 
+	//pop from stack 0
 	mystack.pop(0)
-	assert.Equal(t, mystack.items, []int{1, 0, 0, 0, 2, 5, 0, 0, 3, 0, 0, 0})
+	expected = []int{1, 0, 0, 0, 2, 5, 0, 0, 3, 0, 0, 0}
+	assert.Equal(t, mystack.items, expected)
 
+	//pop from stack 2
 	mystack.pop(2)
-	assert.Equal(t, mystack.items, []int{1, 0, 0, 0, 2, 5, 0, 0, 0, 0, 0, 0})
+	expected = []int{1, 0, 0, 0, 2, 5, 0, 0, 0, 0, 0, 0}
+	assert.Equal(t, mystack.items, expected)
 
+	//push into stack 2
 	mystack.push(2, 6)
-	assert.Equal(t, mystack.items, []int{1, 0, 0, 0, 2, 5, 0, 0, 6, 0, 0, 0})
-
+	expected = []int{1, 0, 0, 0, 2, 5, 0, 0, 6, 0, 0, 0}
+	assert.Equal(t, mystack.items, expected)
 }
