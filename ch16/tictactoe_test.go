23084b088b7db0638d6b3370930e43ea02930e50
jeshocarmel <jeshocarmel@gmail.com>
2020-01-30 15:25:31 +0800

    added soln for 3.4

--- ch16/tictactoe_test.go
+++ ch16/tictactoe_test.go
@@ -5,7 +5,31 @@ import "testing"
 import "github.com/magiconair/properties/assert"
 
 func TestTicTacToe(t *testing.T) {
-	arr := [][]int{{0, 1, 1}, {0, 0, 1}, {1, 0, 0}}
-	result := hasWon(arr, 1, 1)
-	assert.Equal(t, result, 0)
+
+	/* case 1
+
+	x o x
+	o x o
+	x o x
+
+	winner - x
+
+	*/
+
+	arr := [][]int{{1, 0, 1}, {0, 1, 0}, {1, 0, 1}}
+	result := hasWon(arr, 2, 0)
+	assert.Equal(t, result, 1)
+
+	/* case 2
+
+	x o o
+	o x x
+	x x o
+
+	result = no winner
+	*/
+
+	arr = [][]int{{1, 0, 0}, {0, 1, 1}, {1, 1, 0}}
+	result = hasWon(arr, 0, 2)
+	assert.Equal(t, result, -1)
 }
